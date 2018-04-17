334f6fc53821d51f35ea89a4f0bc1e66eb4ecb17
Yewbdar <Yewbdar.grima@gmail.com>
2018-04-17 13:39:58 -0500

    added View Low Inventory function

--- bamazonManager.js
+++ bamazonManager.js
@@ -14,17 +14,7 @@ connection.connect(function (err) {
     // console.log(`connected as id ${connection.threadId}`);
 });
 
-function queryAllProducts() {
-    connection.query("SELECT * FROM Products", function (err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + "|" + res[i].stock_quantity);
-        }
-        console.log("-----------------------------------");
 
-    });
-    connection.end();
-
-}
 start();
 function start() {
     inquirer
@@ -32,21 +22,71 @@ function start() {
             name: "choice",
             type: "list",
             message: "select the option",
-            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+            choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product","exit"]
         })
         .then(function (answer) {
             // based on their answer, either call the bid or the post functions
             if (answer.choice === "View Products for Sale") {
-                postAuction();
+                queryAllProducts();
             }
-            else if (answer.choice === "View Products for Sale") {
-                bidAuction();
+            else if (answer.choice === "View Low Inventory") {
+                lowInventory()
             }
             else if (answer.choice === "Add to Inventory") {
-                bidAuction();
+
             }
             else if (answer.choice === "Add New Product") {
-                bidAuction();
+
+            }
+            else if (answer.choice === "exit") {
+                 process.exit();
             }
         });
+}
+function queryAllProducts() {
+    connection.query("SELECT * FROM Products", function (err, res) {
+        if (res.length > 0) {
+            for (var i = 0; i < res.length; i++) {
+                console.log("\n" + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + "|" + res[i].stock_quantity);
+            }
+            console.log("-----------------------------------");
+        } else {
+            console.log("Data not found !")
+        }
+    });
+  
+    console.log("hi");
+    start();
+
+
+}
+function lowInventory() {
+    connection.query("SELECT * FROM Products WHERE stock_quantity < 5",
+        function (err, res) {
+            console.log(res.length)
+            if (res.length > 0) {
+                for (var i = 0; i < res.length; i++) {
+                    console.log("\n" + res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + "|" + res[i].stock_quantity);
+                }
+                console.log("-----------------------------------");
+            } else {
+                console.log("Data not found !")
+            }
+
+        });
+        start();
+}
+function chooseAgain() {
+    inquirer.prompt({
+        name: "again",
+        type: "confirm",
+        message: "Would you like to continue ?"
+    }).then(function (answer) {
+        if (answer.again === true) {
+            input();
+        } else {
+
+            process.exit();
+        }
+    });
 }
